a1cdca2d8a89918eca088f18a76c652e7e3433f4
A B <ab218@live.com>
2019-11-06 10:40:17 -0800

    analysis button styles, separate analysis buttons code out of spreadsheet

--- src/Spreadsheet.js
+++ src/Spreadsheet.js
@@ -9,6 +9,7 @@ import ContextMenu from './ContextMenu';
 import DistributionModal from './ModalDistribution';
 import FilterModal from './ModalFilter';
 import ColumnTypeModal from './ModalColumnType';
+import AnalysisButtons from './AnalysisButtons';
 import Row from './Row';
 import { SelectedCell } from './Cell';
 import {
@@ -20,36 +21,7 @@ import {
   MODIFY_CURRENT_SELECTION_CELL_RANGE,
   SELECT_CELL,
   OPEN_CONTEXT_MENU,
-  TOGGLE_ANALYSIS_MODAL,
-  TOGGLE_DISTRIBUTION_MODAL,
-  TOGGLE_FILTER_MODAL
 } from './constants'
-import { Button, Icon, Tooltip } from 'antd';
-
-function AnalysisButtons() {
-  const dispatchSpreadsheetAction = useSpreadsheetDispatch();
-  return (
-    <div style={{position: 'fixed', bottom: 20, justifyContent: 'center', width: '100%', display: 'flex', height: 45}}>
-      <div style={{marginTop: 3}}>
-        <Tooltip title='Distribution'>
-          <Button onClick={() => dispatchSpreadsheetAction({type: TOGGLE_DISTRIBUTION_MODAL, distributionModalOpen: true })} style={{height: 40, fontSize:26}}>
-            <Icon rotate={90} type="box-plot" />
-          </Button>
-        </Tooltip>
-      </div>
-      <div style={{marginLeft: 20, marginTop: 3}}>
-        <Tooltip title='Fit Y by X'>
-          <Button onClick={() => dispatchSpreadsheetAction({type: TOGGLE_ANALYSIS_MODAL, analysisModalOpen: true })} style={{height: 40, fontSize:26}} icon="line-chart" />
-        </Tooltip>
-      </div>
-      <div style={{marginLeft: 20, marginTop: 3}}>
-        <Tooltip title='Filter'>
-          <Button onClick={() => dispatchSpreadsheetAction({type: TOGGLE_FILTER_MODAL, filterModalOpen: true, selectedColumns: [] })} style={{height: 40, fontSize:26}} icon="filter" />
-        </Tooltip>
-      </div>
-    </div>
-  )
-}
 
 // function FormulaBar() {
 //   return (
