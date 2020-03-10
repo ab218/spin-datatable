5351a540232068c33d837648f59fb45eed2d6890
Kenny Teng <provpup@users.noreply.github.com>
2020-03-10 16:33:48 -0700

    Always start with at least one column

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SpreadsheetProvider } from './SpreadsheetProvider';
 import eventBus from './EventBusSubscriptions.js';
 
 function App({tableData = {}}) {
-	const {columns = [], rows = [], excludedRows = [], ...rest} = tableData;
+	const {columns = [{ id: '_abc1_', modelingType: 'Continuous', type: 'Number', label: 'Column 1' }], rows = [], excludedRows = [], ...rest} = tableData;
 	return (
 		<div style={{ height: '100%' }} className="App">
 			<SpreadsheetProvider eventBus={eventBus} initialTable={{columns, rows, excludedRows, ...rest}}>
