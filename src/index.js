dc48f5d68a6139b526bca8a2de18783ec6cac6f9
Kenny Teng <provpup@users.noreply.github.com>
2020-03-10 16:28:38 -0700

    Test React rendering first

--- src/index.js
+++ src/index.js
@@ -20,21 +20,21 @@ const artifactID = getTableID();
 
 function openTable({configuration, dependencyKeys, dependencies, viewDisabled}) {
   // TODO: Use configuration in some way
-  const openTableRequestOptions = {type: 'aggregate-event', eventName: 'open-table', event: {id: artifactID}};
-  console.debug('sending open table request with options:', openTableRequestOptions);
-  chrome.runtime.sendMessage(openTableRequestOptions, function renderTable(tableProperties) {
-    if (chrome.runtime.lastError) {
-      console.error('Something went wrong opening table:', chrome.runtime.lastError);
-      alert(`Something went wrong opening table: ${chrome.runtime.lastError.message}`);
-    } else {
-      console.debug('open-table response:', tableProperties);
-      ReactDOM.render(<App tableData={tableProperties}/>, document.getElementById('root'));
+  // const openTableRequestOptions = {type: 'aggregate-event', eventName: 'open-table', event: {id: artifactID}};
+  // console.debug('sending open table request with options:', openTableRequestOptions);
+  // chrome.runtime.sendMessage(openTableRequestOptions, function renderTable(tableProperties) {
+  //   if (chrome.runtime.lastError) {
+  //     console.error('Something went wrong opening table:', chrome.runtime.lastError);
+  //     alert(`Something went wrong opening table: ${chrome.runtime.lastError.message}`);
+  //   } else {
+  //     console.debug('open-table response:', tableProperties);
+      ReactDOM.render(<App />, document.getElementById('root'));
       // If you want your app to work offline and load faster, you can change
       // unregister() to register() below. Note this comes with some pitfalls.
       // Learn more about service workers: https://bit.ly/CRA-PWA
       serviceWorker.unregister();
-    }
-  });
+  //   }
+  // });
 }
 
 if (artifactID) {
