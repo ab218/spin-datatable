cc85bbd8679bcc4748a04eb424914b56c7d2f0f0
A B <ab218@live.com>
2020-09-17 17:53:21 -0700

    refactor sidebar into smaller components

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -111,20 +111,21 @@ const SidebarColumn = React.memo(function({ column, columnIndex, rows }) {
 	);
 });
 
-export default React.memo(function Sidebar() {
-	const {
-		columns,
-		rows,
-		appliedFilterExclude,
-		appliedFilterInclude,
-		excludedRows,
-		dataTableName,
-		savedFilters,
-	} = useRowsState();
-	const { filterModalOpen } = useSpreadsheetState();
+const ExcludedRowsCounter = React.memo(function() {
+	const { excludedRows, includedRows, rows } = useRowsState();
+	const totalExcludedWithIncludedRows = rows.length - includedRows.length;
+	const totalExcludedRows = excludedRows.length;
+	return (
+		<tr>
+			<td style={{ width: '80%' }}>Excluded</td>
+			<td style={{ width: '20%' }}>{includedRows.length ? totalExcludedWithIncludedRows : totalExcludedRows}</td>
+		</tr>
+	);
+});
+
+const SavedFilters = React.memo(function() {
+	const { columns, rows, savedFilters } = useRowsState();
 	const dispatchSelectAction = useSelectDispatch();
-	const dispatchSpreadsheetAction = useSpreadsheetDispatch();
-	const dispatchRowsAction = useRowsDispatch();
 	const [ filterClicked, setFilterClicked ] = useState(null);
 
 	useEffect(
@@ -139,140 +140,165 @@ export default React.memo(function Sidebar() {
 	);
 
 	return (
+		savedFilters.length > 0 && (
+			<React.Fragment>
+				<table style={{ userSelect: 'none', marginLeft: '10px', width: '100%' }}>
+					<tbody>
+						<tr>
+							<td style={{ width: '80%', fontWeight: 'bold' }}>Filters</td>
+							<td style={{ width: '20%', fontWeight: 'bold' }} />
+						</tr>
+						{savedFilters.map((filter) => (
+							<SidebarFilter filterClicked={filterClicked} filter={filter} setFilterClicked={setFilterClicked} />
+						))}
+					</tbody>
+				</table>
+				<Divider />
+			</React.Fragment>
+		)
+	);
+});
+
+const SidebarFilter = React.memo(function({ filter, filterClicked, setFilterClicked }) {
+	const { appliedFilterExclude, appliedFilterInclude } = useRowsState();
+	const { filterModalOpen } = useSpreadsheetState();
+	const dispatchSelectAction = useSelectDispatch();
+	const dispatchSpreadsheetAction = useSpreadsheetDispatch();
+	const dispatchRowsAction = useRowsDispatch();
+	const { filteredRowIDs, filterName, id, numberFilters, selectedColumns, script, stringFilters } = filter;
+	const checked = filterClicked && filterClicked.id === id;
+	return (
+		<React.Fragment key={id}>
+			<tr
+				className={checked ? 'sidebar-column-selected' : ''}
+				onClick={() => {
+					setFilterClicked(() => (checked ? null : filter));
+				}}
+				onContextMenu={(e) => {
+					e.preventDefault();
+					dispatchSpreadsheetAction({
+						type: OPEN_CONTEXT_MENU,
+						contextMenuType: 'cellSelectionRule',
+						contextMenuPosition: { left: e.pageX, top: e.pageY },
+						filter,
+					});
+				}}
+				onMouseOver={() => {
+					dispatchRowsAction({
+						type: HIGHLIGHT_FILTERED_ROWS,
+						filteredRowIDs,
+					});
+				}}
+				onMouseOut={() => {
+					if (!filterModalOpen) {
+						dispatchRowsAction({
+							type: REMOVE_HIGHLIGHTED_FILTERED_ROWS,
+						});
+					}
+				}}
+				onDoubleClick={() => {
+					dispatchSelectAction({ type: SET_SELECTED_COLUMN, selectedColumns });
+					dispatchSpreadsheetAction({ type: TOGGLE_FILTER_MODAL, filterModalOpen: true });
+					dispatchRowsAction({
+						type: SET_FILTERS,
+						selectedColumns,
+						id,
+						filterName,
+						stringFilters,
+						numberFilters,
+						script,
+					});
+				}}
+			>
+				<td>{filterName || script || 'Filter'}</td>
+				<td>
+					{appliedFilterInclude === filter.id ? (
+						<CheckCircleOutlined style={{ color: 'green', marginRight: 20 }} />
+					) : null}
+					{appliedFilterExclude === filter.id ? <StopOutlined style={{ color: 'red', marginRight: 20 }} /> : null}
+				</td>
+			</tr>
+			<tr style={{ height: '10px' }} />
+		</React.Fragment>
+	);
+});
+
+const SidebarColumns = React.memo(function() {
+	const { columns, rows } = useRowsState();
+	return (
+		<table style={{ userSelect: 'none', marginTop: '100px', marginLeft: '10px', width: '100%' }}>
+			<tbody>
+				<tr>
+					<td style={{ fontWeight: 'bold' }}>Columns</td>
+				</tr>
+				{columns &&
+					columns.map((column, columnIndex) => (
+						<SidebarColumn key={columnIndex} column={column} columnIndex={columnIndex} rows={rows} />
+					))}
+			</tbody>
+		</table>
+	);
+});
+
+const SidebarRows = React.memo(function() {
+	const { rows } = useRowsState();
+	return (
+		<table style={{ userSelect: 'none', marginLeft: '10px', width: '100%' }}>
+			<tbody>
+				<tr>
+					<td style={{ width: '80%', fontWeight: 'bold' }}>Rows</td>
+					<td style={{ width: '20%', fontWeight: 'bold' }} />
+				</tr>
+				<tr>
+					<td style={{ width: '80%' }}>All Rows</td>
+					<td style={{ width: '20%' }}>{rows.length}</td>
+				</tr>
+				<SelectedRowsCounter />
+				<ExcludedRowsCounter />
+			</tbody>
+		</table>
+	);
+});
+
+const SidebarTableName = React.memo(function() {
+	const { dataTableName } = useRowsState();
+	const dispatchSelectAction = useSelectDispatch();
+	const dispatchRowsAction = useRowsDispatch();
+	return (
+		<Input
+			style={{ borderLeft: 'none', borderTop: 'none', borderRight: 'none', borderRadius: 0 }}
+			size="large"
+			placeholder="Untitled Data Table"
+			defaultValue={dataTableName}
+			onClick={(e) => {
+				dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
+			}}
+			onKeyDown={(e) => {
+				if (e.key === 'Enter') {
+					e.target.blur();
+				}
+			}}
+			onBlur={(e) => {
+				dispatchRowsAction({ type: SET_TABLE_NAME, dataTableName: e.target.value });
+			}}
+		/>
+	);
+});
+
+export default function Sidebar() {
+	return (
 		<div
 			style={{
 				height: '100%',
 				width: '95%',
 			}}
 		>
-			<Input
-				style={{ borderLeft: 'none', borderTop: 'none', borderRight: 'none', borderRadius: 0 }}
-				size="large"
-				placeholder="Untitled Data Table"
-				defaultValue={dataTableName}
-				onClick={(e) => {
-					dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
-				}}
-				onKeyDown={(e) => {
-					if (e.key === 'Enter') {
-						e.target.blur();
-					}
-				}}
-				onBlur={(e) => {
-					dispatchRowsAction({ type: SET_TABLE_NAME, dataTableName: e.target.value });
-				}}
-			/>
-			<table style={{ userSelect: 'none', marginTop: '100px', marginLeft: '10px', width: '100%' }}>
-				<tbody>
-					<tr>
-						<td style={{ fontWeight: 'bold' }}>Columns</td>
-					</tr>
-					{columns &&
-						columns.map((column, columnIndex) => (
-							<SidebarColumn key={columnIndex} column={column} columnIndex={columnIndex} rows={rows} />
-						))}
-				</tbody>
-			</table>
+			<SidebarTableName />
+			<SidebarColumns />
 			<Divider />
-			<table style={{ userSelect: 'none', marginLeft: '10px', width: '100%' }}>
-				<tbody>
-					<tr>
-						<td style={{ width: '80%', fontWeight: 'bold' }}>Rows</td>
-						<td style={{ width: '20%', fontWeight: 'bold' }} />
-					</tr>
-					<tr>
-						<td style={{ width: '80%' }}>All Rows</td>
-						<td style={{ width: '20%' }}>{rows.length}</td>
-					</tr>
-					<SelectedRowsCounter />
-					<tr>
-						<td style={{ width: '80%' }}>Excluded</td>
-						<td style={{ width: '20%' }}>{excludedRows.length}</td>
-					</tr>
-				</tbody>
-			</table>
+			<SidebarRows />
 			<Divider />
-			{savedFilters.length > 0 && (
-				<React.Fragment>
-					<table style={{ userSelect: 'none', marginLeft: '10px', width: '100%' }}>
-						<tbody>
-							<tr>
-								<td style={{ width: '80%', fontWeight: 'bold' }}>Filters</td>
-								<td style={{ width: '20%', fontWeight: 'bold' }} />
-							</tr>
-							{savedFilters.map((filter) => {
-								const {
-									filteredRowIDs,
-									filterName,
-									id,
-									numberFilters,
-									selectedColumns,
-									script,
-									stringFilters,
-								} = filter;
-								const checked = filterClicked && filterClicked.id === id;
-								return (
-									<React.Fragment key={id}>
-										<tr
-											className={checked ? 'sidebar-column-selected' : ''}
-											onClick={() => {
-												setFilterClicked(() => (checked ? null : filter));
-											}}
-											onContextMenu={(e) => {
-												e.preventDefault();
-												dispatchSpreadsheetAction({
-													type: OPEN_CONTEXT_MENU,
-													contextMenuType: 'cellSelectionRule',
-													contextMenuPosition: { left: e.pageX, top: e.pageY },
-													filter,
-												});
-											}}
-											onMouseOver={() => {
-												dispatchRowsAction({
-													type: HIGHLIGHT_FILTERED_ROWS,
-													filteredRowIDs,
-												});
-											}}
-											onMouseOut={() => {
-												if (!filterModalOpen) {
-													dispatchRowsAction({
-														type: REMOVE_HIGHLIGHTED_FILTERED_ROWS,
-													});
-												}
-											}}
-											onDoubleClick={() => {
-												dispatchSelectAction({ type: SET_SELECTED_COLUMN, selectedColumns });
-												dispatchSpreadsheetAction({ type: TOGGLE_FILTER_MODAL, filterModalOpen: true });
-												dispatchRowsAction({
-													type: SET_FILTERS,
-													selectedColumns,
-													id,
-													filterName,
-													stringFilters,
-													numberFilters,
-													script,
-												});
-											}}
-										>
-											<td>{filterName || script || 'Filter'}</td>
-											<td>
-												{appliedFilterInclude === filter.id ? (
-													<CheckCircleOutlined style={{ color: 'green', marginRight: 20 }} />
-												) : null}
-												{appliedFilterExclude === filter.id ? (
-													<StopOutlined style={{ color: 'red', marginRight: 20 }} />
-												) : null}
-											</td>
-										</tr>
-										<tr style={{ height: '10px' }} />
-									</React.Fragment>
-								);
-							})}
-						</tbody>
-					</table>
-					<Divider />
-				</React.Fragment>
-			)}
+			<SavedFilters />
 		</div>
 	);
-});
+}
