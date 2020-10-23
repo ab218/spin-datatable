f3e8663bb1f5d7485f1490601635768beaa03f8f
A B <ab218@live.com>
2020-10-22 22:00:18 -0700

    refactor toolbar

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -17,73 +17,94 @@ import {
   TOGGLE_FILTER_MODAL,
 } from "./constants";
 export default function Toolbar() {
+  return (
+    <div className={"toolbar"}>
+      <FilterButton />
+      <DescriptiveAnalysisButton />
+      <GraphBuilderButton />
+      <FitYByXButton />
+    </div>
+  );
+}
+
+function FilterButton() {
   const dispatchSpreadsheetAction = useSpreadsheetDispatch();
   const dispatchSelectAction = useSelectDispatch();
   return (
-    <div className={"toolbar"}>
-      <div
-        onClick={() => {
-          dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
-          dispatchSpreadsheetAction({
-            type: TOGGLE_FILTER_MODAL,
-            filterModalOpen: true,
-          });
-        }}
-        className={"toolbar-button"}
-      >
-        <FilterTwoTone
-          className={"toolbar-icon"}
-          twoToneColor={"yellowgreen"}
-        />
-        <div>Filter</div>
-      </div>
-      <div
-        onClick={() => {
-          dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
-          dispatchSpreadsheetAction({
-            type: TOGGLE_DISTRIBUTION_MODAL,
-            distributionModalOpen: true,
-          });
-        }}
-        className={"toolbar-button"}
-      >
-        <BoxPlotTwoTone
-          className={"toolbar-icon"}
-          twoToneColor={"green"}
-          rotate={90}
-        />
-        <div>Descriptive</div>
-      </div>
-      <div
-        onClick={() => {
-          dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
-          dispatchSpreadsheetAction({
-            type: TOGGLE_BAR_CHART_MODAL,
-            barChartModalOpen: true,
-            selectedColumns: [],
-          });
-        }}
-        className={"toolbar-button"}
-      >
-        <BarChartOutlined className={"toolbar-icon"} style={{ color: "red" }} />
-        <div>Graph Builder</div>
-      </div>
-      <div
-        onClick={() => {
-          dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
-          dispatchSpreadsheetAction({
-            type: TOGGLE_ANALYSIS_MODAL,
-            analysisModalOpen: true,
-          });
-        }}
-        className={"toolbar-button"}
-      >
-        <LineChartOutlined
-          className={"toolbar-icon"}
-          style={{ color: "blue" }}
-        />
-        <div>Fit Y By X</div>
-      </div>
+    <div
+      onClick={() => {
+        dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
+        dispatchSpreadsheetAction({
+          type: TOGGLE_FILTER_MODAL,
+          filterModalOpen: true,
+        });
+      }}
+      className={"toolbar-button"}
+    >
+      <FilterTwoTone className={"toolbar-icon"} twoToneColor={"yellowgreen"} />
+      <div>Filter</div>
+    </div>
+  );
+}
+function DescriptiveAnalysisButton() {
+  const dispatchSpreadsheetAction = useSpreadsheetDispatch();
+  const dispatchSelectAction = useSelectDispatch();
+  return (
+    <div
+      onClick={() => {
+        dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
+        dispatchSpreadsheetAction({
+          type: TOGGLE_DISTRIBUTION_MODAL,
+          distributionModalOpen: true,
+        });
+      }}
+      className={"toolbar-button"}
+    >
+      <BoxPlotTwoTone
+        className={"toolbar-icon"}
+        twoToneColor={"green"}
+        rotate={90}
+      />
+      <div>Descriptive</div>
+    </div>
+  );
+}
+function GraphBuilderButton() {
+  const dispatchSpreadsheetAction = useSpreadsheetDispatch();
+  const dispatchSelectAction = useSelectDispatch();
+  return (
+    <div
+      onClick={() => {
+        dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
+        dispatchSpreadsheetAction({
+          type: TOGGLE_BAR_CHART_MODAL,
+          barChartModalOpen: true,
+          selectedColumns: [],
+        });
+      }}
+      className={"toolbar-button"}
+    >
+      <BarChartOutlined className={"toolbar-icon"} style={{ color: "red" }} />
+      <div>Graph Builder</div>
+    </div>
+  );
+}
+function FitYByXButton() {
+  const dispatchSpreadsheetAction = useSpreadsheetDispatch();
+  const dispatchSelectAction = useSelectDispatch();
+  return (
+    <div
+      onClick={() => {
+        dispatchSelectAction({ type: REMOVE_SELECTED_CELLS });
+        dispatchSpreadsheetAction({
+          type: TOGGLE_ANALYSIS_MODAL,
+          analysisModalOpen: true,
+        });
+      }}
+      className={"toolbar-button"}
+    >
+      <LineChartOutlined className={"toolbar-icon"} style={{ color: "blue" }} />
+      <div>Fit Y By X</div>
     </div>
   );
 }
